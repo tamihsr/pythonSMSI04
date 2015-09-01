419729173c59ff3c4e7a9b93305d14e6e3138418
tamihsr <tammy.septiana@gmail.com>
2015-09-01 15:17:32 +0700

    first commit

---
+++ coba.py
@@ -0,0 +1,4 @@
+print "Selamat Datang :)"
+nama = raw_input ("Masukan Nama Anda: ")
+umur = input ("Masukan Umur Anda: ")
+print "Hai {0}, umur {1}". format (nama,umur)
---
+++ luas segitiga.py
@@ -0,0 +1,4 @@
+print "Program Menghitung Luas Segitiga"
+a = input ("Masukan Alas: ")
+t = input ("Masukan Tinggi: ")
+print "Luas Segitiga adalah {0}". format (a*t/2)
